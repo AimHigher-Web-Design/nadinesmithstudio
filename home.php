bd164b033a5bc1d754e8602515dceb46af8e7629
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-02-05 20:01:12 +0800

    added home menu

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @package Nadine Smith Studio
+ */
+
+/*
+Template Name: Home Page
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?wp_nav_menu( array( 'theme_location' => 'home-menu', 'container_class' => 'home_nav' ) ); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
