b98e055480391c448f812e1bc52b51e6697f2d36
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-02-05 19:03:41 +0800

    added new theme files

--- footer.php
+++ footer.php
@@ -1,7 +1,37 @@
-<?php ?>
-  <footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  	<script src="/wordpress/wp-content/themes/glenrockhay/resources/js/main.js"></script>
-  </footer>
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Nadine Smith Studio
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+			<nav class="social-navigation" role="navigation">
+				<?php
+					wp_nav_menu( array(
+						'theme_location'  => 'social',
+						'container_class' => 'menu-social',
+						'menu_class'      => 'clear',
+						'link_before'     => '<span class="screen-reader-text">',
+						'link_after'      => '</span>',
+						'depth'           => 1,
+					) );
+				?>
+			</nav><!-- .social-navigation -->
+		<?php endif; ?>
+		<div class="site-info">
+			<span class="sep"> | </span>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,66 +1,37 @@
-<?php ?><!DOCTYPE html>
-<html>
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Nadine Smith Studio
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-  <!-- Getting stuff working right -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Page Details -->
-  <title><?php echo esc_url(home_url('/')) ?></title>
-  <meta name="description" content="">
-  <meta name="author" name="AimHigher Web Design" />
-  <link rel="shortcut icon" href="resources/favicon/favicon.png" type="image/png" />
-
-  <!-- Google and Verification stuff -->
-  <meta name="google-site-verification" content="7aP4YkOX2QZW8rm4M3_RlhZ_0h_djkKQezuid4EWH8Q" />
-
-  <!-- Social Media Data -->
-  <meta property="og:site_name" content="">
-  <meta property="og:url" content="">
-  <meta property="og:title" content="">
-  <meta property="og:description" content="">
-  <meta property="og:image" content="">
-
-  <!-- Twitter Card -->
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="">
-  <meta name="twitter:url" content="">
-  <meta name="twitter:title" content="">
-  <meta name="twitter:description" content="">
-  <meta name="twitter:image" content="">
-
-  <!-- External stylesheets, files and libraries -->
-  <link rel="stylesheet" href="/wordpress/wp-content/themes/glenrockhay/resources/css/main.css">
-  <link href="" rel="stylesheet">
-  <script src="/wordpress/wp-content/themes/glenrockhay/node_modules/masonry.pkgd.js"></script>
-  <script src="/wordpress/wp-content/themes/glenrockhay/node_modules/packery.pkgd.js"></script>
-  <script src="/wordpress/wp-content/themes/glenrockhay/node_modules/draggabilly.pkgd.js"></script>
+<?php wp_head(); ?>
 </head>
-<body>
-  <header>
-    <nav>
-      <div class="nav-item">
-          <a href="/wordpress/wp-content/themes/glenrockhay/home" class="menu-item">Home</a>
-      </div>
-      <div class="nav-item">
-        <a href="/wordpress/wp-content/themes/glenrockhay/gallery" class="menu-item">Gallery</a>
-      </div>
-      <div class="nav-item">
-        <a href="" class="menu-item">Inquiries</a>
-      </div>
-      <div class="nav-item">
-        <a href="" class="menu-item">Products</a>
-      </div>
-      <div class="nav-item">
-        <a href="" class="menu-item">News</a>
-      </div>
-      <div class="nav-item">
-        <a href="" class="menu-item">Articles</a>
-      </div>
-    </nav>
-    <div class="site-logo">
-      <a href="/wordpress/wp-content/themes/glenrockhay/home"><img src="/wordpress/wp-content/themes/glenrockhay/resources/images/logos/site-logo.png" class="page-logo" /></a>
-    </div>
-  </header>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'boardwalk' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<?php boardwalk_the_site_logo(); ?>
+			<div class="clear">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div>
+		</div><!-- .site-branding -->
+		<?php if ( is_active_sidebar( 'sidebar-1' ) || has_nav_menu( 'primary' ) ) : ?>
+			<button class="sidebar-toggle" aria-expanded="false" ><span class="screen-reader-text"><?php _e( 'Toggle Sidebar', 'boardwalk' ); ?></span></button>
+		<?php endif; ?>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
