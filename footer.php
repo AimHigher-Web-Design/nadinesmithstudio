09d48345cdd94dab3469dc973a09ee2dd8b0f7d7
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-02-19 17:22:58 +0800

    re-added files

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Nadine Smith Studio
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+			<nav class="social-navigation" role="navigation">
+				<?php
+					wp_nav_menu( array(
+						'theme_location'  => 'social',
+						'container_class' => 'menu-social',
+						'menu_class'      => 'clear',
+						'link_before'     => '<span class="screen-reader-text">',
+						'link_after'      => '</span>',
+						'depth'           => 1,
+					) );
+				?>
+			</nav><!-- .social-navigation -->
+		<?php endif; ?>
+		<div class="site-info">
+			<span class="sep"> | </span>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Nadine Smith Studio
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<link rel="stylesheet" type="text/css" href="/wp-content/themes/nadinesmithstudio/resources/css/main.css" />
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'boardwalk' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<?php boardwalk_the_site_logo(); ?>
+			<div class="clear">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div>
+		</div><!-- .site-branding -->
+		<?php if ( is_active_sidebar( 'sidebar-1' ) || has_nav_menu( 'primary' ) ) : ?>
+			<button class="sidebar-toggle" aria-expanded="false" ><span class="screen-reader-text"><?php _e( 'Toggle Sidebar', 'boardwalk' ); ?></span></button>
+		<?php endif; ?>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
