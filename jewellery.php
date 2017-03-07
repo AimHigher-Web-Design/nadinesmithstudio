19972ef8529c28c7a82113dff68173ae0d18fa95
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-03-07 22:26:20 +0800

    products are now working with filters

--- jewellery.php
+++ jewellery.php
@@ -18,21 +18,45 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div class="site-main">
 
+			<form action="" method="GET" class="filter-buttons">
+				<span>Filter by:</span>
+				<input type="submit" value="Jewellery" name="jewellery-type"></input>
+				<input type="submit" value="Bracelet" name="jewellery-type"></input>
+				<input type="submit" value="Necklace" name="jewellery-type"></input>
+				<input type="submit" value="Earrings" name="jewellery-type"></input>
+			</form>
+
 			<div class="products category-jewellery">
 
 				<?php
-			$type = 'etsy_products';
-			$args=array(
-			  'post_type' => $type,
-			  'post_status' => 'publish',
-			  'posts_per_page' => -1,
-			  'caller_get_posts'=> 1,
-				'etsy_category' => 'Bracelet'
-			);
-			$my_query = null;
-			$my_query = new WP_Query($args);
-			if( $my_query->have_posts() ) {
-			  while ($my_query->have_posts()) : $my_query->the_post(); ?>
+					$prodCat = "Jewellery";
+
+					// echo $prodCat.before;
+
+					if (isset($_GET["jewellery-type"])) {
+						$prodCat = $_GET["jewellery-type"];
+						// echo $prodCat.assigned;
+					};
+
+					// echo $prodCat.assignedafter;
+
+					// echo $_POST[3].name;
+
+					// echo $prodCat.cat;
+
+					$type = 'etsy_products';
+
+					$args=array(
+			  		'post_type' => $type,
+			  		'post_status' => 'publish',
+			  		'posts_per_page' => -1,
+			  		'caller_get_posts'=> 1,
+						'etsy_category' => $prodCat
+					);
+					$my_query = null;
+					$my_query = new WP_Query($args);
+					if( $my_query->have_posts() ) {
+			  		while ($my_query->have_posts()) : $my_query->the_post(); ?>
 
 					<div class="product-item jewellery">
 						<div class="product-item-image">
@@ -47,12 +71,12 @@ get_header(); ?>
 					</div>
 
 			    <?php
-			  endwhile;
-			}
-			wp_reset_query();  // Restore global post data stomped by the_post().
-			?>
+			  		endwhile;
+					}
+					wp_reset_query();  // Restore global post data stomped by the_post().
+					?>
 
-		</div>
+				</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
