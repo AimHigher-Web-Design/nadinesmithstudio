fc2f0ff1442ea82d7a568e102a9d1b732481e060
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-05-13 17:03:55 +0800

    done most of the development changes

--- jewellery.php
+++ jewellery.php
@@ -80,7 +80,23 @@ get_header(); ?>
 
 				</div>
 
-		</main><!-- #main -->
+			<div class="page-content">
+				<?php if ( have_posts() ) : ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						*/
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+			</div>
+		</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
